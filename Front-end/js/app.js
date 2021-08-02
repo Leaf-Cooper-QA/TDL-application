5ed1d7ec3405945d0af835237a9f93bfc50a2eb5
Leaf-Cooper-QA <85402946+Leaf-Cooper@users.noreply.github.com>
2021-08-02 11:06:08 +0100

    added js skeleton

---
+++ Front-end/js/app.js
@@ -0,0 +1,41 @@
+(function(){
+    let getallbtn = document.querySelector("#getallbtn");
+    let getonebtn = document.querySelector("#getonebtn");
+    let postbtn = document.querySelector("#postbtn");
+    let putbtn = document.querySelector("#putbtn");
+    let deletebtn = document.querySelector("#deletebtn");
+
+    let getid = document.querySelector("#getoneid");
+    let post1 = document.querySelector("#post1");
+    let post2 = document.querySelector("#post2");
+    let putid = document.querySelector("#putid");
+    let put1 = document.querySelector("#put1");
+    let put2 = document.querySelector("#put2");
+    let deleteid = document.querySelector("#deleteid");
+
+    let myGetAll = function() {
+
+    }
+    getallbtn.addEventListener("click",() => myGetAll(), false);
+
+    let myGetOne = function() {
+
+    }
+    getonebtn.addEventListener("click",() => myGetOne(), false);
+
+    let myPost = function() {
+
+    }
+    postbtn.addEventListener("click",() => myPost(), false);
+
+    let myPut = function() {
+
+    }
+    putbtn.addEventListener("click",() => myPut(), false);
+
+    let myDelete = function() {
+
+    }
+    deletebtn.addEventListener("click",() => myDelete(), false);
+
+})();
