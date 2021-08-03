aca2a81fa2bf3d16ddca77a5598445ebf2c7bbdf
Leaf-Cooper-QA <85402946+Leaf-Cooper@users.noreply.github.com>
2021-08-03 15:12:30 +0100

    added connections from front end to back end

--- Back-end/Spring-TDL/src/main/java/com/qa/SpringTDL/rest/TodoController.java
+++ Back-end/Spring-TDL/src/main/java/com/qa/SpringTDL/rest/TodoController.java
@@ -5,8 +5,10 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -16,6 +18,7 @@ import com.qa.SpringTDL.domain.Todo;
 import com.qa.SpringTDL.service.TodoService;
 
 @RestController
+@CrossOrigin
 public class TodoController {
 	
 	private TodoService service;
@@ -30,10 +33,12 @@ public class TodoController {
 		return new ResponseEntity<>(this.service.getall(), HttpStatus.OK);
 	}
 	
-	@GetMapping("/getone")
-	public ResponseEntity<Todo> getone(@RequestBody Long id) {
-		return new ResponseEntity<>(this.service.getone(id), HttpStatus.OK);
-	}
+	
+	//getone is being obnoxious, leave it be for the time being
+//	@GetMapping("/getone/{id}")
+//	public ResponseEntity<Todo> getone(@PathVariable("id") Long id) {
+//		return new ResponseEntity<>(this.service.getone(id), HttpStatus.OK);
+//	}
 	
 	@PostMapping("/post")
 	public ResponseEntity<Todo> post(@RequestBody Todo todo) {
@@ -46,8 +51,9 @@ public class TodoController {
 	}
 	
 	@PutMapping("/put")
-	public ResponseEntity<Todo> update(@RequestBody Todo todo, @RequestBody Long id) {
-		return new ResponseEntity<>(this.service.update(id, todo), HttpStatus.OK);
+	//The update method is slightly cheaty because we're passing in the id as part of the item, don't worry about it
+	public ResponseEntity<Todo> update(@RequestBody Todo todo) {
+		return new ResponseEntity<>(this.service.update(todo), HttpStatus.OK);
 	}
 
 }
--- Front-end/js/index-app.js
+++ Front-end/js/index-app.js
@@ -1,11 +1,11 @@
 (function(){
     let getallbtn = document.querySelector("#getallbtn");
-    let getonebtn = document.querySelector("#getonebtn");
+    /*let getonebtn = document.querySelector("#getonebtn");*/
     let postbtn = document.querySelector("#postbtn");
     let putbtn = document.querySelector("#putbtn");
     let deletebtn = document.querySelector("#deletebtn");
 
-    let getid = document.querySelector("#getoneid");
+    /*let getid = document.querySelector("#getoneid");*/
     let post1 = document.querySelector("#post1");
     let post2 = document.querySelector("#post2");
     let post3 = document.querySelector("#post3");
@@ -21,29 +21,68 @@
     /* URL is local host stuff defined by java controller */
 
     let myGetAll = function() {
+        fetch(`http://localhost:8080/getall`, {
+            method: `get`})
+            .then((data) =>{
+                /*fill data into table*/
+            })
+            .catch(err => console.log(`something went wrong with error message: ${err}`)) 
 
     }
     getallbtn.addEventListener("click",() => myGetAll(), false);
 
+/*
     let myGetOne = function() {
+        fetch(`http://localhost:8080/getone/${getid}`, {
+            method:`get`})
+            .then((data) =>{
+                
+            })
+            .catch(err => console.log(`something went wrong with error message: ${err}`)) 
 
     }
     getonebtn.addEventListener("click",() => myGetOne(), false);
-
+*/
     let myPost = function() {
+        fetch(`http://localhost:8080/post`, {
+            method:`post`,
+            headers: {"Content-type": "application/json; charset=UTF-8"}, 
+            body: JSON.stringify({
+                name: post1.value,
+                description: post2.value,
+                time: post3.value
+            })
+        })
+        .then((data) => {
+            /*fill data into table*/
+        })
 
     }
     postbtn.addEventListener("click",() => myPost(), false);
 
     let myPut = function() {
+        fetch(`http://localhost:8080/put`, {
+            method: `put`,
+            headers: {"Content-type": "application/json; charset=UTF-8"}, 
+            body: JSON.stringify({
+                id: putid.value,
+                name: put1.value,
+                description: put2.value,
+                time: put3.value
+            })
+        })
 
     }
     putbtn.addEventListener("click",() => myPut(), false);
 
     let myDelete = function() {
         
-        fetch(`https://dummywebsite.com/data/${deleteid.value}`, {
-            method: `delete`})
+        /*this is the correct sorta thing for the url but details might be wrong*/
+        fetch(`http://localhost:8080/delete`, {
+            method: `delete`,
+            headers: {"Content-type": "application/json; charset=UTF-8"},  
+            body:JSON.stringify(deleteid.value)  
+        })
             .then((data) =>{
                  /*deletes automatically, return data to the table*/
                 deletepara.append(document.createTextNode(`data with id: ${deleteid.value} deleted`));
